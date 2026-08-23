a2cc97e104bcb7799fc6704150df45a2819e716e
Medin Turkes <medin_93@live.com>
2026-08-24 00:16:37 +0200

    Remove backend from frontend

--- DailyGourmet.Api/Authentication/IJwtTokenService.cs
+++
@@ -1,8 +0,0 @@
-using DailyGourmet.Api.Models.Entities;
-
-namespace DailyGourmet.Api.Authentication;
-
-public interface IJwtTokenService
-{
-    string GenerateToken(User user);
-}
--- DailyGourmet.Api/Options/CorsOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace DailyGourmet.Api.Options;
-
-public class CorsOptions
-{
-    public const string SectionName = "Cors";
-
-    public string[] AllowedOrigins { get; set; } = [];
-}
